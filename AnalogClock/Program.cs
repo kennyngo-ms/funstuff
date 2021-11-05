3c2d440bccb63195c803bfb773bec5d4641bad75
kennyngo-ms <68084191+kennyngo-ms@users.noreply.github.com>
2021-11-05 14:45:06 -0400

    add analog clock

---
+++ AnalogClock/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AnalogClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = "09:15";
+
+            int hours = 0; int minutes = 0;
+            float hourHandAngle = 0; float minuteHandAngle;
+
+            //Console.WriteLine(time.Substring(0, 2) + " " + time.Substring(3, 2));
+            hours =Int32.Parse(time.Substring(0,2))%12;
+            minutes = Int32.Parse(time.Substring(3,2));
+            Console.WriteLine(hours + " " + minutes);
+
+            minuteHandAngle = (float)minutes / 60 * 360;
+            hourHandAngle = (float)hours / 12 * 360 + (minuteHandAngle/360 * 30);
+
+            if(Math.Abs(hourHandAngle - minuteHandAngle) >= 180)
+            {
+                Console.WriteLine("hours angle: " + hourHandAngle + "\nminutes angle: " + minuteHandAngle + "\ninterior angle: " + (360- Math.Abs(hourHandAngle - minuteHandAngle)));
+            }
+            else Console.WriteLine("hours angle: " + hourHandAngle + "\nminutes angle: " + minuteHandAngle + "\ninterior angle: " + Math.Abs(hourHandAngle-minuteHandAngle));
+        }
+    }
+}
--- PalindromeSubstring/Program.cs
+++ PalindromeSubstring/Program.cs
@@ -7,18 +7,18 @@ namespace PalindromeSubstring
     {
         static void Main(string[] args)
         {
-            string testString = "eye bird madam abba";
-            //string testString = RandomString(40);
+            //string testString = "eye bird madam abba";
+            string testString = RandomString(10);
             int minSize = 1;
-            Console.WriteLine(testString);
             List<string> palindromes= new List<string>();
             HashSet<string> uniquePalindromes = new HashSet<string>();
 
             palindromes = ExtractPalindrome(testString);
 
+            Console.WriteLine(testString);
             Console.Write("\nPalindromes: ");
             foreach (string i in palindromes) 
-            {   
+            {
                 //print all palindrome with lenght greater than minSize
                 if (i.Length > minSize) Console.Write(i + "  ");
                 uniquePalindromes.Add(i);
@@ -30,8 +30,6 @@ namespace PalindromeSubstring
                 //print all palindrome with lenght greater than minSize
                 if (i.Length > minSize) Console.Write(i + "  ");
             }
-
-
         }
         
         static List<String> ExtractPalindrome(string tString)
@@ -39,32 +37,31 @@ namespace PalindromeSubstring
             List<string> allPalindromes = new List<string>();
             string tSubstring=null;
             int size = 0;
+            int headerShift = 0;
             int stringLength = tString.Length;
 
             for (int i = 0; i < stringLength; i++)
             {
                 //odd palindromes
                 size = 1;
+                headerShift = 0;
                 tSubstring = tString.Substring(i, size);
-
                 while (CheckPalindrome(tSubstring) && i - (size/2) >= -1 && i + (size/2) < stringLength)
                 {
                     allPalindromes.Add(tSubstring);
-                    size=size+2;
-
-                    if (i - (size / 2)>=0 && i + (size/2) < stringLength) tSubstring = tString.Substring(i-(size/2), size);
-
+                    size = size+2;
+                    if (i - (size / 2) + headerShift >=0 && i + (size/2) < stringLength) tSubstring = tString.Substring(i-(size/2)+headerShift, size);
                 }
 
                 //even palindromes
                 size = 2;
-                if(i+size/2<stringLength) tSubstring = tString.Substring(i, size);
+                headerShift = 1;
+                if (i+size/2<stringLength) tSubstring = tString.Substring(i, size);
                 while (CheckPalindrome(tSubstring) && i - (size / 2) >= -1 && i + (size / 2) < stringLength)
                 {
                     allPalindromes.Add(tSubstring);
                     size = size + 2;
-
-                    if (i - (size / 2)+1 >= 0 && i + (size / 2) < stringLength) tSubstring = tString.Substring(i - (size / 2)+1, size);
+                    if (i - (size / 2) + headerShift >= 0 && i + (size / 2) < stringLength) tSubstring = tString.Substring(i - (size / 2) + headerShift, size);
                 }
             }
             return allPalindromes;
@@ -77,7 +74,6 @@ namespace PalindromeSubstring
             string reverseString = String.Concat(reversechar);
             if (palindrome == reverseString) return true;
             else return false;
-
         }
 
         static bool CheckPalindrome(string palindrome)
@@ -89,14 +85,14 @@ namespace PalindromeSubstring
 
         private static string RandomString(int length = 20)
         {
-            string characters = "ABCD";
+            string characters = "ABCDEFG";
             Random randomint = new Random();
             char tempChar;
             string randomString="";
 
             for (int i=0; i < length; i++)
             {
-                tempChar = characters[randomint.Next(0, 4)];
+                tempChar = characters[randomint.Next(0, 7)];
                 randomString += tempChar;
             }
             return randomString;
